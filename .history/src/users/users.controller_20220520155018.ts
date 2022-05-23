e89988f0cf0506268d3a1e8325bdc26416ba8c5d
Joan Romero <joan.romero@patagonian.it>
2022-05-23 15:24:53 -0300

    some progress connection to DB

---
+++ .history/src/users/users.controller_20220520155018.ts
@@ -0,0 +1,9 @@
+import { Controller, Post } from '@nestjs/common';
+
+@Controller('auth')
+export class UsersController {
+  @Post('/signup')
+  createUser() {
+    console.log('createUser');
+  }
+}
